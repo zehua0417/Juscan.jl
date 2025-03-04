415308574c44502a52cfee06b013d3d1de7f1ffa
Documenter.jl <documenter@juliadocs.github.io>
2025-03-04 12:45:23 +0000

    build based on c85afe3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"tutorial/quickstart/#todo","page":"Quick Start","title":"todo","text":"","category":"section"},{"location":"#Juscan","page":"Home","title":"Juscan","text":"","category":"section"}]
+}
