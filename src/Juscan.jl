a096950abb9ba56a55f269403f4be113a374fbfa
LiZehua <zehuali0417@gmail.com>
2025-03-04 16:12:52 +0800

    init

---
+++ src/Juscan.jl
@@ -0,0 +1,12 @@
+#=========================================
+filename: Juscan
+author: Lihuax
+date: 2025-03-04 16:12
+description: Main module for Juscan
+=========================================#
+
+module Juscan
+
+greet() = print("Hello World!")
+
+end # module Juscan
